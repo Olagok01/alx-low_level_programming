9c50cf19a4f5f9a9512ef59253373271256379c0
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-16 10:34:51 -0700

    my commit file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,7 @@ int main(void)
 	printf("Size of char: %c byte(s)\n", sizeof(a));
 	printf("Size of int: %d byte(s)\n", sizeof(b));
 	printf("Size of long int %lu byte(s)\n", sizeof(c));
-	printf("Size of long long int %lld byte(s)\n", sizeof(d));
-	printf("Size of float %f byte(s)\n", sizeof(e));
+	printf("Size of long long int %lu byte(s)\n", sizeof(d));
+	printf("Size of float %lu byte(s)\n", sizeof(e));
+	return (0);
 }
