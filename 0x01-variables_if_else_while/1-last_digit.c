a62eec719fe0d686b9ee70ac4e88598b7d12902f
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-17 04:20:56 -0700

    my commit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -15,7 +16,7 @@ int main(void)
 	L = n % 10;
 	if (L > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n , L);
+		printf("Last digit of %d is %d and is greater than 5\n", n, L);
 	} else if (L == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, L);
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char low;
+	int low;
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
