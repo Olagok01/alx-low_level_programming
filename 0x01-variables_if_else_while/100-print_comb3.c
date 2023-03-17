a67cb8561720e8a4f6c1c1fd14c2451226ba6621
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-17 14:04:06 -0700

    my commit file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <studio.h>
+
+/**
+ * main - Entry point
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int num_1 = 0;
+	int num_2 = 1;
+
+	for (num_1 = 0; num_1 <= 9; num_1++)
+	{
+		for (num_2 = 0; num_2 <= 9; num_2++)
+		{
+			if (num_2 > num_1)
+			{
+				putchar('0' + num_1);
+				putchar('0' + num_2);
+				if (!(num_1 == 9 && num_2 == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
