748025c2e9ecc961a908ed191c8b696cf2a86f77
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-17 17:57:50 -0700

    my commit file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,22 @@ int main(void)
 
 	for (num_1 = 0; num_1 <= 99; num_1++)
 	{
-		for(num_2 = 0l num_2 <= 99; num_2++)
+		for(num_2 = l; num_2 <= 99; num_2++)
 		{
-			putchar('0' + num_1);
-			putchar('0' + nm_2);
+			if (num_2 > num_1)
+			{
+				putchar((num_1 / 10) + '0');
+				putchar((num_1 % 10) + '0');
+				putchar(' ');
+				putchar((num_2 / 10) + '0');
+				putchar((num_2 % 10) + '0');
+				if (!(num_1 == 98 && num_2 == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
