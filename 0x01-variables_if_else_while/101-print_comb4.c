e908e734557897b6ab4e3b354b44776d704c26f0
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-17 17:28:55 -0700

    my commit file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible different combinations of three digits.
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int num_1 = 0;
+	int num_2 = 0;
+	int num_3 = 3;
+
+	for (num_1 = 0; num_1 <= 9; num_1++)
+	{
+		for (num_2 = 0; num_2 <= 9; num_2++)
+		{
+			for (num_3 = 0; num_3 <= 9; num_3++)
+			{
+				if (num_2 > num_1)
+				{
+					if(num_3 > num_2)
+					{
+						putchar('0' + num_1);
+                                                putchar('0' + num_2);
+                                                putchar('0' + num_3);
+						if(!(num_1 == 7 && num_2 == 8 && num_3 == 9))
+						{
+							putchar(',');
+                                                	putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
