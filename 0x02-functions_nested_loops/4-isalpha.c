8550ba31c5c6f4829fae73dfad46e97b18c4bd69
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-21 16:03:10 -0700

    my commit file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** int _isalpha - unction that checks for alphabetic character.
+ * @c: single letter inputs
+ * Return: 1 if int c is upper or lower case, 0 if otherwise
+ */
+
+
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
