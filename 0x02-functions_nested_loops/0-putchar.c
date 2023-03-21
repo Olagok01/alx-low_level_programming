94d980aea2025a3f1cc76ed3e7e32a69b01deaae
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-21 12:43:32 -0700

    my commit file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - Write a program that prints _putchar, followed by a new line.
- * Return: Alwayss (0) Success
+ * main - prints _putchar, followed by a new line.
+ * Return: Always (0) Success
  */
 
 int main(void)
