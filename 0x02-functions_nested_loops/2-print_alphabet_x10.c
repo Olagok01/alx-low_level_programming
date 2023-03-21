3f6aa86a8ca458b2337c50b20de1b67c8e2d5cd9
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-21 15:28:03 -0700

    my commit file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints (10) times the alphabet in lowercase.
+ * Return: Always (0) Success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char low = 'a';
+		while (a <= 'z')
+		{
+			_putchar(low);
+			low++;
+		}
+	}
+	-putchar('\n');
+}
