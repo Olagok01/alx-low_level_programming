3f225176f7a69e102f7dc417f86229bc160d2e7c
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-21 18:05:47 -0700

    my commit file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,16 @@
 void times_table(void)
 {
 	int a = 0;
-	int b = 1;
+	int b;
 	int result;
 
 	while (a < 10)
 	{
+		b = 1;
 
 		while (b < 10)
 		{
-			if (b !=9)
+			if (b != 9)
 			{
 				result = (a * b);
 				_putchar(result + '0');
