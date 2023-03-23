2697e33a0c594c80937726e02b6e9b7b1c2a80a1
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-23 14:43:36 -0700

    my commit file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal.
+ * @n: the int for the paramaters of my function
+ * Return: 0
+ */
 
 void print_diagonal(int n)
 {
