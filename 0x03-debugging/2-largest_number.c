2096f8606e006702fa7bd3c0d650a226fa54974b
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-23 10:21:13 -0700

    my commit file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,6 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -25,9 +26,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-	largest = c;
- }
-
-return (largest);
+		largest = c;
+	}
+	return (largest);
 }
-
