c4c65736827ddf3a62a8abceda4e2fd952a7e7cf
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-23 12:21:07 -0700

    my commit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for digit from 0 - 9.
+ * @c: the int for the paramaters of my function
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
