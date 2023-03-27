1e3930b351765fd63cd1cea316e1fb5cb34f6f5c
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-27 16:39:47 -0700

    my commit file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - func that prints 1 char/2 of a string, followed by a new line.
+ * @str: declaration of str and paramters for the function puts2
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+	char c;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			c = s[i];
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}	
