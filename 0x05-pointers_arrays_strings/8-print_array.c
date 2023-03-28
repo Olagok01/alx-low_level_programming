6c1fef0fb67d6a4cefa5883da86828db88e245d1
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-27 17:27:16 -0700

    my commit file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - Prints an inputted number of elements
+ *               of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= (n - 1); i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	printf("\n");
+}
