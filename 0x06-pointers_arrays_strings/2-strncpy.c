ba0422d7ca42e9a193fa1427ade4afe8087a178f
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-29 15:14:42 -0700

    my commit file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int length = 0;
 
-	for (i = 0; src[i] != 0; i++)
-	{
-		length++;
-	}
 	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
