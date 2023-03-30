3e2d5f14a748abed600d74e298094b5aad05c585
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-03-30 09:03:27 -0700

    my commit file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,37 +7,30 @@
 
 void print_number(int n)
 {
-        unsigned int num = n;
+	unsigned int num = n;
 
-        if (n < 0)
-        {
-                _putchar('-');
-                num = -num;
-        }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-        if (num >= 1000 && num <= 9999)
-        {
-                _putchar((num / 1000) + '0');
-                _putchar (((num / 100) % 10) + '0');
-                _putchar(((num / 10) % 10) + '0');
-        }
+	if (num >= 1000 && num <= 9999)
+	{
+		_putchar((num / 1000) + '0');
+		_putchar (((num / 100) % 10) + '0');
+		_putchar(((num / 10) % 10) + '0');
+	}
 
-        if (num >= 100 && num <= 999)
-        {
-                _putchar((num / 100) + '0');
+	if (num >= 100 && num <= 999)
+	{
+		_putchar((num / 100) + '0');
+		_putchar(((num / 10) % 10) + '0');
+	}
 
-
-                _putchar(((num / 10) % 10) + '0');
-        }
-
-
-        if (num >= 10 && num <= 99)
-        {
-                _putchar((num / 10) + '0');
-        }
-
-
-        _putchar((num % 10) + '0');
-
-
+	if (num >= 10 && num <= 99)
+	{
+		_putchar((num / 10) + '0');
+	}
+	_putchar((num % 10) + '0');
 }
