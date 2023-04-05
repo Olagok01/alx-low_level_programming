e427767d2a440db55739e92e315956a0d0a052a2
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-05 15:44:48 -0700

    my commit file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to print.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+}
