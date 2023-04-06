31a7618e8c9b2d2e5b1e7ea8990312b209eb2c65
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-05 17:50:48 -0700

    my commit file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main."
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: number to raise
+ * @y: the power
+ * Return: value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int answer = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	anwser *= (x * (_pow_recursion(x, y-1)));
+	return (anwser);
+}
