3005f582dd1a3c390c5c9838cc3d852a7da98d4e
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-07 19:29:44 -0700

    my commit file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: 0 on success
  */
 
-int main(int __attribute__((__unused__)) rgc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
