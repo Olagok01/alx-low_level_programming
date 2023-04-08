bb22155ecb9ba9ae56f90c34b9d6cce83db1ba75
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-07 19:19:52 -0700

    my commit file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of argument
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
