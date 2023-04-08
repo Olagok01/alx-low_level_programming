8cd77a6220191d57df955076fc0f560ab8d2ada0
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-08 16:28:03 -0700

    my commit file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,8 +5,8 @@
 /**
  * main - Prints the addition of positive numbers,
  *	followed by a new line.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
+ * @argc: number of arguments in the program.
+ * @argv: array of pointers to the arguments.
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  *	Otherwise - 0.
  */
