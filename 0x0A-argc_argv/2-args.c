b85a99db80022e01fc1d10c6c5882c11e12f9034
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-08 13:34:41 -0700

    my commit file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - print each argument passed to program on a line
+ * @argc: counts he argument
+ * @argv: pointer to array of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
