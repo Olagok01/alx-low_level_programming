79f37afff90df5c5079e2b006c2086ccf904d30d
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-11 13:53:37 -0700

    my commit file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,25 +14,25 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i = 0;
-	
+
 	if (size <= 0)
 	{
 		return (NULL);
 	}
-	
+
 	str = malloc(sizeof(*str) * size);
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	while (i < size)
 	{
 		str[i] = c;
 		i++;
 	}
 	str[i] = '\0';
-	
+
 	return (str);
 }
