1527c90d8e6661203ca24088a5e79aff43e35b16
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-13 11:57:22 -0700

    my commit file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  function that allocates memory using malloc.
+ * @b: the number of memmroy to be allocated
+ * Return: A pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *y = malloc(b);
+
+	if (y == NULL)
+	{
+		exit(98);
+	}
+	return (y);
+}
