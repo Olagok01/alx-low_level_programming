b9f0123f98eff798b6eb5280c128bb2712c59f5f
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-18 17:03:38 -0700

    my commit file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free instance of struct dog
+ * @d: instance to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name)
+	{
+		free(d->name);
+	}
+	if (d->owner)
+	{
+		free(d->owner);
+	}
+
+	free(d);
+}
