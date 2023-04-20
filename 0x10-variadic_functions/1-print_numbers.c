e46da04f498c1c84e4ea99e19b80dd0b84d82fc8
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-20 15:06:59 -0700

    my commit file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,12 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+
 	va_list(args);
+
 	va_start(args, n);
-	
 	for (i = 0; i < n; i++)
 	{
-		if(i > 0 && separator != NULL)
+		if (i > 0 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
