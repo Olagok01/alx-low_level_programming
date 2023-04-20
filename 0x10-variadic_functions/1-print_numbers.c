7ecfaf0adf06fe5887e8e67f54c1d3e3e49e5c19
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-20 15:11:20 -0700

    my commit file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * print_numbers - Prints numbers, followed by a new line.
@@ -12,8 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
-	va_list(args);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
