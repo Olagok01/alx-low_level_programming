7b034578a789ce4635cf4f41bc1a41b88334633c
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-20 15:51:50 -0700

    my commit file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,11 +13,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		char *str = va_arg(args, char *);
+		str = va_arg(args, char *);
 
 		if (str == NULL)
 		{
@@ -27,9 +28,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		if (i > 0 && separator != NULL)
+
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
 	}
+	va_end(args);
+	printf("\n");
 }
