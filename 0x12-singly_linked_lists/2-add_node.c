3e3429bcd073adae27fcb45abb713bc7d337f6fc
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-27 17:25:41 -0700

    my commit file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,18 +23,18 @@ int _strlen(const char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 	unsigned int length;
 
-	if (new == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	length = _strlen(str);
-	new->str = strdup(str);
-	new->len = length;
-	new->next = (*head);
-	(*head) = new;
+	new_node->str = strdup(str);
+	new_node->len = length;
+	new_node->next = (*head);
+	(*head) = new_node;
 
 	return (*head);
 }
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * int _strlen- returns the length of a string
+ * @str: string parameter
+ * Return: length of string
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node to end of linked list
+ * @head: pointer to pointer to linked list
+ * @str: used to set the str attribute of the new node
+ *
+ * Return: address of linked list of NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp_node;	
+
+	unsigned int length;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	length = _strlen(str);
+	temp_node = *head;
+
+	new->str = strdup(str);
+	new->len = length;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (temp_node->next != NULL)
+	temp_node = temp_node->next;
+	temp_node->next = new;
+	return (*head);
+}
