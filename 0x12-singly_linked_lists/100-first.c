313b0443b80c0799427d9d87e6acf6ae5027e043
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-04-28 14:26:27 -0700

    program to execute a file before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
 #include "lists.h"
 
+/**
+ * execute_before_main - executes this function before
+ *	 main function
+ */
+
 void __attribute__ ((constructor)) execute_before_main()
 {
-	printf("You're beat! and yet, you must allow,\n" 
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
