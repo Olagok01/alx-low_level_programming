379495faf323df576f3d1bb5d30ea8c5313b5774
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-05-09 16:26:31 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,10 @@ int create_file(const char *filename, char *text_content)
 			length++;
 		}
 	}
+	else
+	{
+		text_content = "";
+	}
 	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_file = write(open_file, text_content, length);
 
