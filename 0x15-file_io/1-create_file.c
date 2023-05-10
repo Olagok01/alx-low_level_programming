9e5985ad43352b33e2d115317e8eb784e228c99e
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2023-05-09 17:00:09 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length]; length)
-		{
-		}
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
